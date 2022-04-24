5eda93771c51f42a6708f278b51cda817c583827
Leonardo Martinez <shouta.aizawa0709@gmail.com>
2022-04-24 16:47:19 -0500

    upload api

---
+++ app.py
@@ -0,0 +1,59 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Text, Optional
+from datetime import datetime
+from uuid import uuid4 as uuid
+# Run the server with the command-> uvicorn app:app
+app = FastAPI()
+
+posts = []
+
+# Post model
+class Post(BaseModel):
+    id: Optional[str]
+    title: str
+    author:str
+    content: Text
+    created_at :  datetime = datetime.now()
+    published_at: Optional[datetime]
+    published: bool = False
+
+@app.get('/')
+def read_root():
+    return {"Welcome" : "welcome to my api"}
+
+@app.get('/posts')
+def get_posts():
+    return posts
+
+@app.post('/posts')
+def save_posts(post: Post):
+    post.id = str(uuid())
+    #print(post.dict())
+    posts.append(post.dict())
+    return post[-1]
+
+@app.get('/posts/{post_id}')
+def get_post(post_id: str):
+    for post in posts:
+        if post["id"] == post_id:
+            return post
+    return HTTPException(status_code=404, detail="Post no found")
+
+@app.delete('/posts/{post_id}')
+def delete_post(post_id: str):
+    for index, post in enumerate(posts):
+        if post["id"] == post_id:
+            posts.pop(index)
+            return {"message": "Post has been deleted succesfully"}
+    return HTTPException(status_code=404, detail="Post no found")
+
+@app.put('/posts/{post_id}')
+def update_post(post_id: str, updatePost: Post):
+    for index, post in enumerate(posts):
+        if post["id"] == post_id:
+            posts[index]["title"] = updatePost.title
+            posts[index]["content"] = updatePost.content
+            posts[index]["author"] = updatePost.author
+            return {"message": "Post has been updated succesfully"}
+    return HTTPException(status_code=404, detail="Post no found")
